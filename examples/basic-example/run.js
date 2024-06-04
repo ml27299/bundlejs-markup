db5487ba280b79bd451c5018cd61c995277d37d3
Macario Lara <ml27299@gmail.com>
2024-06-04 13:19:03 -0500

    feature added artificial directive support

--- examples/basic-example/run.js
+++ examples/basic-example/run.js
@@ -5,6 +5,7 @@ console.log(markup);
 const { categoriesV2 } = markup;
 
 (async () => {
+	// await categoriesV2({ servicesFilter: { active: true } });
 	const response = await categoriesV2({ servicesFilter: { active: true } });
 	console.log(response);
 })().catch((err) => {
